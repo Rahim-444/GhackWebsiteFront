fd733e56db59a7bc8593d18a2fef67de20e00975
SohaibGhz <sohaib.ghazi@outlook.fr>
2024-02-22 16:11:36 +0100

    finishing navbar, updating global styles

--- src/component/home/Navbar.jsx
+++ src/component/home/Navbar.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import { Button } from "antd";
 
 const Navbar = () => {
   return (
-    <div className="flex justify-between items-center h-12 bg-red-700 mt-8">
-      <h1 className="text-3xl">LOGO</h1>
-      <button>Sign Up</button>
+    <div className="flex justify-between items-center h-12 mt-8">
+      <h1 className="text-3xl font-semibold">LOGO</h1>
+     <button className="bg-button-color text-white rounded-lg py-3 px-8 flex-shrink ">Track your orders</button>
     </div>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,10 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {colors: {
+      'button-color':'#2C2D5B',
+      }},
+    
   },
   plugins: [],
 };
