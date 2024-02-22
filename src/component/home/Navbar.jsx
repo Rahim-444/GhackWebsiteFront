066aa92fb0af83c0a32b20d2562515028ae93941
SohaibGhz <sohaib.ghazi@outlook.fr>
2024-02-22 15:56:34 +0100

    index.css edited

--- src/component/home/Navbar.jsx
+++ src/component/home/Navbar.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Navbar = () => {
   return (
     <div className="flex justify-between items-center h-12 bg-red-700 mt-8">
-      <h1 className="text-[]">LOGO</h1>
+      <h1 className="text-3xl">LOGO</h1>
       <button>Sign Up</button>
     </div>
   );
