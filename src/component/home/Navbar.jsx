2ae21b6962097ff446688029a2d903a9bee30ec0
SohaibGhz <sohaib.ghazi@outlook.fr>
2024-02-22 15:49:41 +0100

    editing index.css for glabal styles

---
+++ src/component/home/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="flex justify-between items-center h-12 bg-red-700 mt-8">
+      <h1 className="text-[]">LOGO</h1>
+      <button>Sign Up</button>
+    </div>
+  );
+};
+
+export default Navbar;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,12 @@
 import React from "react";
-
-
-
-
+import Navbar from "../component/home/Navbar";
 
 const Home = () => {
-  return <div>
-    hello world
-  </div>;
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
 };
 
 export default Home;
